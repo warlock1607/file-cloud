3a4e12f36a3906fd87406fc817ab2166241c9232
Vladimir Zhelvis <war1lok@yandex.ru>
2019-05-15 22:06:56 +0300

    add keycloak adapter

--- src/server.js
+++ src/server.js
@@ -1,11 +1,37 @@
 const express = require("express");
-
-const app = express();
+const expressHbs = require('express-handlebars');
+const session = require('express-session');
+const Keycloak = require('keycloak-connect');
 
 const { port } = require("./config");
 
 const router = require("./app/router");
 
-app.use("/", router);
+const app = express();
+
+// Register 'handelbars' extension with The Mustache Express
+app.engine('hbs', expressHbs({extname:'hbs',
+  defaultLayout:'layout.hbs',
+  relativeTo: __dirname
+}));
+
+app.set('view engine', 'hbs');
+
+
+var memoryStore = new session.MemoryStore();
+var keycloak = new Keycloak({ store: memoryStore });
+
+app.use(session({
+  secret:'thisShouldBeLongAndSecret',
+  resave: false,
+  saveUninitialized: true,
+  store: memoryStore
+}));
+
+app.use(keycloak.middleware());
+
+app.use("/", keycloak.protect(), router);
+
+app.use( keycloak.middleware( { logout: '/logout'} ));
 
 app.listen(port, () => console.log(`App started at port ${port}`));
